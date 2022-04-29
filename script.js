6d353f382e7cc7fd719570129d902710e856918f
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2022-04-29 17:32:47 -0300

    primeiro commit do portifolio

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+function olaMundo(nome) {
+    alert("Ola " + nome);
+}*/
+
+function mudarDeCor(){
+    /* codigo */
+    document.getElementsByClassName("muda-cor").style.color = "blue";
+    
+}
